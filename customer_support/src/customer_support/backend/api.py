e7be25956e569cd8f6d50a2e51939c5865d79c42
Josefin Lesley <josefin3647@gmail.com>
2026-04-20 11:44:45 +0200

    Added code

---
+++ customer_support/src/customer_support/backend/api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from customer_support.backend.agents import support_agent
+from customer_support.backend.middlewwares import logging_middleware
+
+app = FastAPI()
+logging_middleware(app=app)
+
+# We use get for simplicity -> in real cases use post
+@app.get("/customer_support")
+async def customer_support_faq(question: str) -> str:
+    result = await support_agent.run(question)
+    return result.output
